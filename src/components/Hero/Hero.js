7c2b3d7cd558de1f8fcd6ced346720b93e83c304
= <mukherjee4004@gmail.com>
2022-06-07 12:03:08 +0530

    change hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
-import { pagego } from '../../constants/pagego';
-import { ExternalLinks } from '../Projects/ProjectsStyles'
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+import { pagego } from "../../constants/pagego";
+import { ExternalLinks } from "../Projects/ProjectsStyles";
 const Hero = (props) => (
- 
   <>
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
-          Welcome To  <br />
-          My Personal Portfolio
+          Welcome To <br />
+          My Personal Portfolio @2022
         </SectionTitle>
         <SectionText>
-         You are here happy to see you ! on a mission 
-         to making a path that glows the world with Technologies😎
+          You are here happy to see you ! on a mission to making a path that
+          glows the world with Technologies😎
         </SectionText>
         <ExternalLinks href={pagego.v}>Click Here</ExternalLinks>
       </LeftSection>
